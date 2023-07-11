6d8fb53d235e1ef017ee2c91c75e2ac31807fd9f
Rodrigo Rocha <rocha.rodrigogomes@gmail.com>
2023-07-11 10:45:21 -0300

    remove configurações do spring cloud gateway e eureka, add configs para novo gateway

---
+++ gateway-service/src/main/java/br/ufrn/dimap/collaborativecanvas/gatewayservice/controller/PlayerServiceController.java
@@ -0,0 +1,7 @@
+package br.ufrn.dimap.collaborativecanvas.gatewayservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PlayerServiceController {
+}
